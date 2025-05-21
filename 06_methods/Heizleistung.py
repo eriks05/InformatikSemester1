5fa8d7c460ed42d3fe952dff7969a3d4bc3da757
eriks05 <erik.schott@mail.de>
2025-05-21 14:25:15 +0000

    .

--- 06_methods/Heizleistung.py
+++ 06_methods/Heizleistung.py
@@ -1,8 +1,8 @@
-b = float(input("Bitte geben Sie die Breite Ihres Raumes in m ein: "))
-t = float(input("Bitte geben Sie die Tiefe Ihres Raumes in m ein: "))
-h = float(input("Bitte geben Sie die Höhe Ihres Raumes in m ein: "))
-T1 = float(input("Bitte geben Sie die Außentemperatur in °C ein: "))
-T2 = float(input("Bitte geben Sie die Innentemperatur in °C ein: "))
+b = float(input("Bitte geben Sie die Breite Ihres Raumes [m] ein: "))
+t = float(input("Bitte geben Sie die Tiefe Ihres Raumes [m] ein: "))
+h = float(input("Bitte geben Sie die Höhe Ihres Raumes [m] ein: "))
+T1 = float(input("Bitte geben Sie die Außentemperatur [°C] ein: "))
+T2 = float(input("Bitte geben Sie die Innentemperatur [°C] ein: "))
 
 V = b * t * h
 dT = T2 - T1
